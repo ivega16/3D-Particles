9f060fbc95cf63295958913e1f5763d3cfe1430b
Isa Vega <imvega100@gmail.com>
2023-10-23 23:17:59 -0400

    created base code, made edits to movement + color

--- sketch.js
+++ sketch.js
@@ -1,7 +1,77 @@
+particles = []
+
 function setup() {
-  createCanvas(400, 400);
+  createCanvas(windowWidth, windowHeight, WEBGL)
+  angleMode(DEGREES)
 }
 
 function draw() {
-  background(220);
+  background(0, 0, 30);
+
+  //add movement by rotating
+  rotateX
+
+  directionalLight([255], createVector(0, 0, -1))
+
+  if(random(1) > 0.97) {
+
+    var x = random(-100, 100)
+    var y = random(-100, 100)
+    var z = random(-100, 100)
+
+    var pos = createVector(x, y, z)
+
+
+    for(var i = 0 ; i < 100; i++) {
+
+      //adding randomness to colors
+      var r  = map(sin(frameCount), -1, 1, 0, 255) + random(-50, 50)
+      var r  = map(sin(frameCount /2), -1, 1, 255, 0) + random(-50, 50)
+      var r  = map(sin(frameCount /4), -1, 1, 0, 255) + random(-50, 50)
+
+      //create color object and pass to Particle obj
+      var c = color (r, g, b)
+
+      var p = new Particle(pos, c)
+      particles.push(p)
+    }
+  }
+
+  for (var i = particles.length - 1; i >=0; i--) {
+    if (dist(particles[i].pos.x, particles[i].pos.y, particles[i].pos.z, 0, 0, 0) < 500) {
+      particles[i].update()
+      particles[i].show()
+    } else {
+      particles.splice(i, 1)
+    }
+  }
+}
+
+
+class Particle {
+  constructor (pos, c) {
+    this.pos =  createVector(pos.x, pos.y, pos.z)
+    this.vel = p5.Vector.random3D().normalize().mult(random(4, 6))
+
+    this.c = c
+
+    //create property that controls width of particle
+    this.w = random(4, 10)
+  }
+  update() {
+    this.pos.add(this.vel)
+  }
+  show() {
+    push ()
+
+    noStroke()
+    fill(this.c)
+
+    translate(this.pos.x, this.pos.y, this.pos.z)
+    sphere(this.w)
+
+
+    pop()
+
+  }
 }
